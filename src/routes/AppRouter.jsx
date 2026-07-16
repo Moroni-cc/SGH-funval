8ffbfddb1ae8847a7a80dd894791c09ebba0011b
Fabian Coloma <fabianaca17.123@gmail.com>
2026-07-16 09:26:48 -0500

    feat: agregar vistas de perfil, cambiar contraseña y configurar rutas protegidas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,6 +4,8 @@ import NotFound from "../pages/shared/NotFound";
 import Unauthorized from "../pages/shared/Unauthorized";
 import AdminDashboard from "../pages/admin/Dashboard";
 import StudentDashboard from "../pages/student/Dashboard";
+import Profile from "../pages/profile/Profile";
+import ChangePassword from "../pages/profile/ChangePassword";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
 import RoleRoute from "./RolRoute";
@@ -13,19 +15,32 @@ function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
+      
       <Route element={<PublicRoute />}>
         <Route path="/login" element={<Login />} />
       </Route>
+      
       <Route path="/unauthorized" element={<Unauthorized />} />
 
+     
       <Route element={<ProtectedRoute />}>
+        
+        {/* Solo Admin */}
         <Route element={<RoleRoute allowedRoles={[ROLES.ADMIN]}/>}>
           <Route path="/admin" element={<AdminDashboard />} />
         </Route>
 
+        {/* Solo Student */}
         <Route element={<RoleRoute allowedRoles={[ROLES.STUDENT]} />}>
           <Route path="/student" element={<StudentDashboard />} />
         </Route>
+
+        {/* 2. TU PARTE: Accesible para ADMIN y STUDENT */}
+        <Route element={<RoleRoute allowedRoles={[ROLES.ADMIN, ROLES.STUDENT]} />}>
+          <Route path="/perfil" element={<Profile />} />
+          <Route path="/perfil/password" element={<ChangePassword />} />
+        </Route>
+
       </Route>
 
       <Route path="*" element={<NotFound />} />
